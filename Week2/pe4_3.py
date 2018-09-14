7606c08baa241c2507f0b10c8a5542347a4cb42b
Clijmart <mart.clijnk@student.hu.nl>
2018-09-14 10:37:47 +0200

    Add | Opdrachten 1-3 van Les4 (Met testregel voor Git)

--- Week2/pe4_3.py
+++ Week2/pe4_3.py
@@ -2,3 +2,6 @@ uurloon = float(input('Yo, wat verdien je per uur? \nUurloon: '))
 uren = int(input('Hoeveel uur heb je gewerkt? \nUren: '))
 
 print('Wow, je salaris is €' + str(round(uurloon * uren, 2)) + ' na ' + str(uren) + ' uur werken!')
+
+
+# Extra regel als test voor dit git ding :D
