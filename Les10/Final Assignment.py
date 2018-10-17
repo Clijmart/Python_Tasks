b190543bdb6d32b5e963e77e41b5bbe7f8da9b3a
Clijmart <mart.clijnk@student.hu.nl>
2018-10-17 13:32:03 +0200

    Add Les10 (FA)

---
+++ Les10/Final Assignment.py
@@ -0,0 +1,42 @@
+def inlezen_beginstation(stations):
+    while True:
+        beginstation = input("Wat is je beginstation? ")
+        if beginstation in stations:
+            break
+        else:
+            print("Deze trein komt niet in " + beginstation + ".")
+    return beginstation
+
+
+def inlezen_eindstation(stations, beginstation):
+    while True:
+        eindstation = input("Wat is je eindstation? ")
+        if eindstation in stations:
+            if stations[eindstation < beginstation]:
+                break
+        print("Deze trein komt niet in " + eindstation + ".")
+    return eindstation
+
+
+def omroepen_reis(stations, beginstation, eindstation):
+    indexEind = stations.index(eindstation)
+    indexBegin = stations.index(beginstation)
+
+    print("Het beginstation {} is het {}e station in het traject.".format(beginstation, indexBegin + 1))
+    print("Het eindstation {} is het {}e station in het traject.".format(eindstation, indexEind + 1))
+    print("De afstand bedraagt {} station(s).".format(indexEind - indexBegin))
+    print("De prijs van het kaartje is {} euro.".format((indexEind - indexBegin) * 5))
+
+    print("\n")
+    print("Jij stapt in de trein in: " + beginstation)
+    for stationnummer in range(1, indexEind - indexBegin):
+        print(" - " + stations[indexBegin + stationnummer])
+    print("Jij stapt uit in: " + eindstation)
+
+
+stations = ["Schagen", "Heerhugowaard", "Alkmaar", "Castricum", "Zaandam", "Amsterdam Sloterdijk", "Amsterdam Centraal",
+            "Amsterdam Amstel", "Utrecht Centraal", "'s-Hertogenbosch", "Eindhoven",
+            "Weert", "Roermond", "Sittard", "Maastricht"]
+beginstation = inlezen_beginstation(stations)
+eindstation = inlezen_eindstation(stations, beginstation)
+omroepen_reis(stations, beginstation, eindstation)
